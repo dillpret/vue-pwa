c38b6edc16996fc9f31117fb14ba6780fcebdd77
Dillon Pretorius <pretorius.dillon@gmail.com>
2019-08-24 19:29:15 +0200

    gh-pages initial commit

---
+++ precache-manifest.f4a2f1c0126c3552e3658e92162a8824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad43eccdf9b11cbdd40a46d0d57ffb0b",
+    "url": "/vue.config.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b689185ee4a429093b07",
+    "url": "/js/chunk-vendors.7bb5c4ad.js"
+  },
+  {
+    "revision": "4036940985510909b7fc",
+    "url": "/js/app.342d27df.js"
+  },
+  {
+    "revision": "11384380a2288ed3246f10b45fd07b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4036940985510909b7fc",
+    "url": "/css/app.1e1fe651.css"
+  }
+];
